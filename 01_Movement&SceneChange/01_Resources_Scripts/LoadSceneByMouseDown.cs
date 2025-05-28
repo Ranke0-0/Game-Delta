af72400ec9e12384c6ce44ef5d8a8f1fd8a71105
Ranke0-0 <chrisalco06@gmail.com>
2025-05-28 23:24:15 +0200

    Added Resources folders

---
+++ 01_Movement&SceneChange/01_Resources_Scripts/LoadSceneByMouseDown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneByMouseDown : MonoBehaviour
+{
+	public string sceneMouseDown;
+	
+	void Start()
+    {
+        Debug.Log("Tete que pasa");
+    }
+    
+	public void OnMouseDown()
+	{
+		if (sceneMouseDown != null)
+		{
+			Debug.Log("Ahora cambio la Scene");
+			SceneManager.LoadScene(sceneMouseDown);
+		}
+		else 
+		{
+			Debug.Log("No me diste una Scene");
+        }
+    }
+}
